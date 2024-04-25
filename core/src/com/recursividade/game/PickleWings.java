fefc15146cce150b6d09eaa2d6f85982a52f001c
Larissa <larissagomes8020@gmail.com>
2024-04-25 09:02:04 -0300

    Agora os hambúrgueres aparecem na tela

--- core/src/com/recursividade/game/PickleWings.java
+++ core/src/com/recursividade/game/PickleWings.java
@@ -7,31 +7,55 @@ import com.badlogic.gdx.utils.ScreenUtils; //importa
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.TimeUtils;
+import java.util.Iterator; //fazer loop com indereços ao invez de com os própios valores.
+
 
 //criar obj
 public class PickleWings extends ApplicationAdapter { //nossa class erdando da class da lgdx
 	SpriteBatch batch; //tipo para reiderisar objetos
-	Texture img, tPepino; //cria o obj
-	private Sprite pepino;
-	private float posX, posY;
-	private final float velocity = 10;
+	Texture img, tPepino, tPicles, tBurguer; //cria o obj
+	private Sprite pepino, picles, burguer;
+	private float posX, posY, velocity, pos_x_picles, pos_y_picles;
+	private Boolean attack;
+	private Array<Rectangle> burguers;
+	private long lastBurguerTime;
 	
 	@Override //instanciar obj
 	public void create () { //construtor, define e atribui os objs
 		batch = new SpriteBatch();
-		img = new Texture("fundo_preto.png");
-		tPepino = new Texture("pepino.png"); //instancia obj
+		img = new Texture("fundo_roxo.png");
+		tPepino = new Texture("pepino_transparente.png"); //instancia obj
 		pepino = new Sprite(tPepino); //é mais fácil movimentar um sprite do que uma textura
-		posX = 0; posY = 0;
+		posX = 0; posY = 0; velocity = 10;
+		tPicles = new Texture("picles.png");
+		picles = new Sprite(tPicles);
+		pos_x_picles = posX; pos_y_picles = posY;
+		attack = false;
+		tBurguer = new Texture("hamburguer.png");
+		burguers = new Array<Rectangle>();
+		lastBurguerTime = 0;
+
+
 	}
 
 	@Override
-	public void render () { //
+	public void render () { // Desenhar na tela
 		this.movePepino();
+		this.movePicles();
+		this.spawnBurguer();
+		this.moveBurguers();
 		ScreenUtils.clear(1, 0, 0, 1);
 		batch.begin();
 		batch.draw(img, 0, 0); //desenhar os objs
+		batch.draw(picles, (pos_x_picles + pepino.getWidth()/2), (pos_y_picles + (pepino.getHeight() / 2) - 15 ));
 		batch.draw(pepino,posX,posY);
+		for(Rectangle burguer: burguers){
+			batch.draw(tBurguer, burguer.x, burguer.y);
+		}
 		batch.end();
 
 	}
@@ -40,6 +64,7 @@ public class PickleWings extends ApplicationAdapter { //nossa class erdando da c
 	public void dispose () { //
 		batch.dispose();
 		img.dispose();
+		tPepino.dispose();
 	}
 
 	private void movePepino(){
@@ -65,6 +90,50 @@ public class PickleWings extends ApplicationAdapter { //nossa class erdando da c
 			}
 		}
 	}
+
+	private void movePicles(){
+		if (Gdx.input.isKeyPressed(Input.Keys.SPACE) && !attack){
+			attack = true;
+			pos_y_picles = posY;
+		}
+		if (attack){
+			if (pos_x_picles < Gdx.graphics.getWidth()){
+				pos_x_picles += 20;
+			}
+			else{
+				pos_x_picles = posX;
+				attack = false;
+			}
+		}
+		else {
+			pos_x_picles = posX;
+			pos_y_picles = posY;
+		}
+
+	}
+
+	//criar vários hamburguers
+	private void spawnBurguer(){										//onde começa e onde termina
+		Rectangle burguer = new Rectangle(Gdx.graphics.getWidth(),MathUtils.random(0, Gdx.graphics.getHeight() - tBurguer.getHeight()), tBurguer.getWidth(), tBurguer.getHeight());
+		burguers.add(burguer);
+		lastBurguerTime = TimeUtils.nanoTime();
+	}
+	private void moveBurguers(){
+
+		if (TimeUtils.nanoTime() - lastBurguerTime > 999999999) {
+			this.spawnBurguer();
+		}
+
+		for(Iterator<Rectangle> iter = burguers.iterator(); iter.hasNext();){
+			Rectangle burguer = iter.next();
+			burguer.x -= (400 * Gdx.graphics.getDeltaTime());
+			if(burguer.x + tBurguer.getWidth() < 0){
+				iter.remove();
+			}
+		}
+	}
+
+
 }
 // Gdx.graphics.getWidth()  - '1280' Pega de maneira dinamica o tamanho da janela
 // pepino.getWidth() - '187' Pega de maneir automatizada o tamanho da instancia/imagem
